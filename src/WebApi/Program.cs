bdfe3989df30865fee0850441677f3d8c888bc30
hungvult <tuanhungvu0912@gmail.com>
2025-11-16 16:49:49 +0700

    WebApi/Program: navigate user to `/swagger` on start

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -6,11 +6,22 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+// Always enable Swagger and Swagger UI so users are redirected to API docs
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Redirect root URL to Swagger UI (so visiting / opens the docs)
+app.Use(async (context, next) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (context.Request.Path == "/")
+    {
+        // Redirect to the Swagger UI landing page
+        context.Response.Redirect("/swagger");
+        return;
+    }
+
+    await next();
+});
 
 app.UseHttpsRedirection();
 
